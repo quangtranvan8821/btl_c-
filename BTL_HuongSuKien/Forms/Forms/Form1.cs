47aab9cfc71790b9cdcb5ba41a22aee1a3bd33d9
manh06 <94583409+manh06@users.noreply.github.com>
2022-04-10 20:11:00 +0700

    Add files via upload

---
+++ BTL_HuongSuKien/Forms/Forms/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_HuongSuKien.Forms
+{
+    public partial class FormQLNV : Form
+    {
+        public FormQLNV()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BTL_HuongSuKien/Forms/Forms/Form3.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace BTL_HuongSuKien.Forms
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelPhongBan = new System.Windows.Forms.Label();
+            this.labelChucVu = new System.Windows.Forms.Label();
+            this.textBoxPhongBan = new System.Windows.Forms.TextBox();
+            this.labelĐiaChi = new System.Windows.Forms.Label();
+            this.labelSĐT = new System.Windows.Forms.Label();
+            this.textBoxChucVu = new System.Windows.Forms.TextBox();
+            this.textBoxĐiaChi = new System.Windows.Forms.TextBox();
+            this.textBoxSĐT = new System.Windows.Forms.TextBox();
+            this.buttonSua = new System.Windows.Forms.Button();
+            this.buttonChiTietHopDong = new System.Windows.Forms.Button();
+            this.buttonBangLuong = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(70, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(653, 246);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // labelPhongBan
+            // 
+            this.labelPhongBan.AutoSize = true;
+            this.labelPhongBan.Location = new System.Drawing.Point(67, 312);
+            this.labelPhongBan.Name = "labelPhongBan";
+            this.labelPhongBan.Size = new System.Drawing.Size(74, 13);
+            this.labelPhongBan.TabIndex = 3;
+            this.labelPhongBan.Text = "ID Phòng Ban";
+            // 
+            // labelChucVu
+            // 
+            this.labelChucVu.AutoSize = true;
+            this.labelChucVu.Location = new System.Drawing.Point(67, 348);
+            this.labelChucVu.Name = "labelChucVu";
+            this.labelChucVu.Size = new System.Drawing.Size(62, 13);
+            this.labelChucVu.TabIndex = 4;
+            this.labelChucVu.Text = "ID Chức Vụ";
+            // 
+            // textBoxPhongBan
+            // 
+            this.textBoxPhongBan.Location = new System.Drawing.Point(158, 305);
+            this.textBoxPhongBan.Name = "textBoxPhongBan";
+            this.textBoxPhongBan.Size = new System.Drawing.Size(165, 20);
+            this.textBoxPhongBan.TabIndex = 11;
+            // 
+            // labelĐiaChi
+            // 
+            this.labelĐiaChi.AutoSize = true;
+            this.labelĐiaChi.Location = new System.Drawing.Point(67, 384);
+            this.labelĐiaChi.Name = "labelĐiaChi";
+            this.labelĐiaChi.Size = new System.Drawing.Size(41, 13);
+            this.labelĐiaChi.TabIndex = 12;
+            this.labelĐiaChi.Text = "Địa Chỉ";
+            // 
+            // labelSĐT
+            // 
+            this.labelSĐT.AutoSize = true;
+            this.labelSĐT.Location = new System.Drawing.Point(67, 420);
+            this.labelSĐT.Name = "labelSĐT";
+            this.labelSĐT.Size = new System.Drawing.Size(75, 13);
+            this.labelSĐT.TabIndex = 13;
+            this.labelSĐT.Text = "Số Điện Thoại";
+            // 
+            // textBoxChucVu
+            // 
+            this.textBoxChucVu.Location = new System.Drawing.Point(158, 341);
+            this.textBoxChucVu.Name = "textBoxChucVu";
+            this.textBoxChucVu.Size = new System.Drawing.Size(165, 20);
+            this.textBoxChucVu.TabIndex = 14;
+            // 
+            // textBoxĐiaChi
+            // 
+            this.textBoxĐiaChi.Location = new System.Drawing.Point(158, 377);
+            this.textBoxĐiaChi.Name = "textBoxĐiaChi";
+            this.textBoxĐiaChi.Size = new System.Drawing.Size(165, 20);
+            this.textBoxĐiaChi.TabIndex = 15;
+            // 
+            // textBoxSĐT
+            // 
+            this.textBoxSĐT.Location = new System.Drawing.Point(158, 413);
+            this.textBoxSĐT.Name = "textBoxSĐT";
+            this.textBoxSĐT.Size = new System.Drawing.Size(165, 20);
+            this.textBoxSĐT.TabIndex = 16;
+            // 
+            // buttonSua
+            // 
+            this.buttonSua.Location = new System.Drawing.Point(498, 300);
+            this.buttonSua.Name = "buttonSua";
+            this.buttonSua.Size = new System.Drawing.Size(110, 36);
+            this.buttonSua.TabIndex = 17;
+            this.buttonSua.Text = "Sửa";
+            this.buttonSua.UseVisualStyleBackColor = true;
+            // 
+            // buttonChiTietHopDong
+            // 
+            this.buttonChiTietHopDong.Location = new System.Drawing.Point(498, 348);
+            this.buttonChiTietHopDong.Name = "buttonChiTietHopDong";
+            this.buttonChiTietHopDong.Size = new System.Drawing.Size(110, 36);
+            this.buttonChiTietHopDong.TabIndex = 18;
+            this.buttonChiTietHopDong.Text = "Chi Tiết Hợp Đồng";
+            this.buttonChiTietHopDong.UseVisualStyleBackColor = true;
+            // 
+            // buttonBangLuong
+            // 
+            this.buttonBangLuong.Location = new System.Drawing.Point(498, 397);
+            this.buttonBangLuong.Name = "buttonBangLuong";
+            this.buttonBangLuong.Size = new System.Drawing.Size(110, 36);
+            this.buttonBangLuong.TabIndex = 19;
+            this.buttonBangLuong.Text = "Lương Nhân Viên";
+            this.buttonBangLuong.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonBangLuong);
+            this.Controls.Add(this.buttonChiTietHopDong);
+            this.Controls.Add(this.buttonSua);
+            this.Controls.Add(this.textBoxSĐT);
+            this.Controls.Add(this.textBoxĐiaChi);
+            this.Controls.Add(this.textBoxChucVu);
+            this.Controls.Add(this.labelSĐT);
+            this.Controls.Add(this.labelĐiaChi);
+            this.Controls.Add(this.textBoxPhongBan);
+            this.Controls.Add(this.labelChucVu);
+            this.Controls.Add(this.labelPhongBan);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form3";
+            this.Text = "Thông Tin Nhân Viên";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelPhongBan;
+        private System.Windows.Forms.Label labelChucVu;
+        private System.Windows.Forms.TextBox textBoxPhongBan;
+        private System.Windows.Forms.Label labelĐiaChi;
+        private System.Windows.Forms.Label labelSĐT;
+        private System.Windows.Forms.TextBox textBoxChucVu;
+        private System.Windows.Forms.TextBox textBoxĐiaChi;
+        private System.Windows.Forms.TextBox textBoxSĐT;
+        private System.Windows.Forms.Button buttonSua;
+        private System.Windows.Forms.Button buttonChiTietHopDong;
+        private System.Windows.Forms.Button buttonBangLuong;
+    }
+}

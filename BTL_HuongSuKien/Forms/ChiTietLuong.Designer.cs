0d705509a1ecdaa15e6a292023e9eebfe33664eb
quangtranvan8821 <quangtrn8821@gmail.com>
2022-04-17 22:02:55 +0700

    add ChiTietLuong form

---
+++ BTL_HuongSuKien/Forms/ChiTietLuong.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace BTL_HuongSuKien.Forms
+{
+    partial class ChiTietLuong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTenNhanVien = new System.Windows.Forms.TextBox();
+            this.labelTenNhanVien = new System.Windows.Forms.Label();
+            this.labelMaPhongBan = new System.Windows.Forms.Label();
+            this.textBoxMaPhongBan = new System.Windows.Forms.TextBox();
+            this.labelTenPhongBan = new System.Windows.Forms.Label();
+            this.textBoxTenPhongBan = new System.Windows.Forms.TextBox();
+            this.labelTenChucVu = new System.Windows.Forms.Label();
+            this.textBoxTenChucVu = new System.Windows.Forms.TextBox();
+            this.dataGridViewBangLuong = new System.Windows.Forms.DataGridView();
+            this.buttonIn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBangLuong)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxTenNhanVien
+            // 
+            this.textBoxTenNhanVien.Location = new System.Drawing.Point(142, 38);
+            this.textBoxTenNhanVien.Name = "textBoxTenNhanVien";
+            this.textBoxTenNhanVien.ReadOnly = true;
+            this.textBoxTenNhanVien.Size = new System.Drawing.Size(359, 20);
+            this.textBoxTenNhanVien.TabIndex = 0;
+            // 
+            // labelTenNhanVien
+            // 
+            this.labelTenNhanVien.AutoSize = true;
+            this.labelTenNhanVien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTenNhanVien.Location = new System.Drawing.Point(28, 38);
+            this.labelTenNhanVien.Name = "labelTenNhanVien";
+            this.labelTenNhanVien.Size = new System.Drawing.Size(108, 20);
+            this.labelTenNhanVien.TabIndex = 1;
+            this.labelTenNhanVien.Text = "Tên nhân viên";
+            // 
+            // labelMaPhongBan
+            // 
+            this.labelMaPhongBan.AutoSize = true;
+            this.labelMaPhongBan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMaPhongBan.Location = new System.Drawing.Point(586, 38);
+            this.labelMaPhongBan.Name = "labelMaPhongBan";
+            this.labelMaPhongBan.Size = new System.Drawing.Size(103, 20);
+            this.labelMaPhongBan.TabIndex = 3;
+            this.labelMaPhongBan.Text = "Mã nhân viên";
+            // 
+            // textBoxMaPhongBan
+            // 
+            this.textBoxMaPhongBan.Location = new System.Drawing.Point(700, 38);
+            this.textBoxMaPhongBan.Name = "textBoxMaPhongBan";
+            this.textBoxMaPhongBan.ReadOnly = true;
+            this.textBoxMaPhongBan.Size = new System.Drawing.Size(196, 20);
+            this.textBoxMaPhongBan.TabIndex = 2;
+            // 
+            // labelTenPhongBan
+            // 
+            this.labelTenPhongBan.AutoSize = true;
+            this.labelTenPhongBan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTenPhongBan.Location = new System.Drawing.Point(28, 102);
+            this.labelTenPhongBan.Name = "labelTenPhongBan";
+            this.labelTenPhongBan.Size = new System.Drawing.Size(86, 20);
+            this.labelTenPhongBan.TabIndex = 7;
+            this.labelTenPhongBan.Text = "Phòng ban";
+            // 
+            // textBoxTenPhongBan
+            // 
+            this.textBoxTenPhongBan.Location = new System.Drawing.Point(142, 102);
+            this.textBoxTenPhongBan.Name = "textBoxTenPhongBan";
+            this.textBoxTenPhongBan.ReadOnly = true;
+            this.textBoxTenPhongBan.Size = new System.Drawing.Size(279, 20);
+            this.textBoxTenPhongBan.TabIndex = 6;
+            // 
+            // labelTenChucVu
+            // 
+            this.labelTenChucVu.AutoSize = true;
+            this.labelTenChucVu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTenChucVu.Location = new System.Drawing.Point(503, 102);
+            this.labelTenChucVu.Name = "labelTenChucVu";
+            this.labelTenChucVu.Size = new System.Drawing.Size(66, 20);
+            this.labelTenChucVu.TabIndex = 9;
+            this.labelTenChucVu.Text = "Chức vụ";
+            // 
+            // textBoxTenChucVu
+            // 
+            this.textBoxTenChucVu.Location = new System.Drawing.Point(617, 102);
+            this.textBoxTenChucVu.Name = "textBoxTenChucVu";
+            this.textBoxTenChucVu.ReadOnly = true;
+            this.textBoxTenChucVu.Size = new System.Drawing.Size(279, 20);
+            this.textBoxTenChucVu.TabIndex = 8;
+            // 
+            // dataGridViewBangLuong
+            // 
+            this.dataGridViewBangLuong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBangLuong.Location = new System.Drawing.Point(32, 177);
+            this.dataGridViewBangLuong.Name = "dataGridViewBangLuong";
+            this.dataGridViewBangLuong.Size = new System.Drawing.Size(864, 199);
+            this.dataGridViewBangLuong.TabIndex = 10;
+            // 
+            // buttonIn
+            // 
+            this.buttonIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIn.Location = new System.Drawing.Point(778, 391);
+            this.buttonIn.Name = "buttonIn";
+            this.buttonIn.Size = new System.Drawing.Size(118, 47);
+            this.buttonIn.TabIndex = 11;
+            this.buttonIn.Text = "In";
+            this.buttonIn.UseVisualStyleBackColor = true;
+            // 
+            // ChiTietLuong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.buttonIn);
+            this.Controls.Add(this.dataGridViewBangLuong);
+            this.Controls.Add(this.labelTenChucVu);
+            this.Controls.Add(this.textBoxTenChucVu);
+            this.Controls.Add(this.labelTenPhongBan);
+            this.Controls.Add(this.textBoxTenPhongBan);
+            this.Controls.Add(this.labelMaPhongBan);
+            this.Controls.Add(this.textBoxMaPhongBan);
+            this.Controls.Add(this.labelTenNhanVien);
+            this.Controls.Add(this.textBoxTenNhanVien);
+            this.Name = "ChiTietLuong";
+            this.Text = "ChiTietLuong";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBangLuong)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTenNhanVien;
+        private System.Windows.Forms.Label labelTenNhanVien;
+        private System.Windows.Forms.Label labelMaPhongBan;
+        private System.Windows.Forms.TextBox textBoxMaPhongBan;
+        private System.Windows.Forms.Label labelTenPhongBan;
+        private System.Windows.Forms.TextBox textBoxTenPhongBan;
+        private System.Windows.Forms.Label labelTenChucVu;
+        private System.Windows.Forms.TextBox textBoxTenChucVu;
+        private System.Windows.Forms.DataGridView dataGridViewBangLuong;
+        private System.Windows.Forms.Button buttonIn;
+    }
+}
